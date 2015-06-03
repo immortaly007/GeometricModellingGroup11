0df20cfdf20905e82f4dc2708d953c6dd5b9b03e
Mick van Gelderen <mickvangelderen@gmail.com>
2015-06-03 13:27:28 +0200

    Moved pdfs to root, added model argument to run.bat/sh

--- Code/run.sh
+++ Code/run.sh
@@ -1,2 +1,2 @@
 JV_JAR=jars/javaview.jar:jars/jvx.jar:jars/vgpapp.jar:.
-java -classpath $JV_JAR -Djava.library.path="dll" -Xmx1024m javaview codebase=. archive.dev=show
+java -classpath $JV_JAR -Djava.library.path="dll" -Xmx1024m javaview codebase=. archive.dev=show model=../models/rockerArm.obj
